894d4abdf453fa7b7a4bc463e7af6b0df4899ab5
xzhuo <yunkzo@gmail.com>
2025-08-26 15:52:00 -0500

    update bedpe parser

--- parse_insertion_bedpe.py
+++ parse_insertion_bedpe.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import re
 
+
 class Read:
     def __init__(self, line):
         self.line = line
@@ -25,23 +26,21 @@ class Read:
         self.any_soft_clip = self.soft_clip_1 or self.soft_clip_2
 
 
-
-
 class Insertion:
     def __init__(self):
         self.chrom = ''
-        self.strand = ''
         self.insertion_strand = ''
         self.up_reads = []
         self.down_reads = []
         self.upstream_boundary = int()
         self.downstream_boundary = int()
         self.soft_clipped_reads = []
-    
+        self.last_pos = int()
+
     def add_read(self, read):
         self.chrom = read.chrom_1
-        self.strand = read.strand_1
         self.insertion_strand = read.insertion_strand
+        self.last_pos = max(self.last_pos, read.end_1)
         
         if read.position == "up":
             self.up_reads.append(read)
@@ -55,14 +54,26 @@ class Insertion:
             if read.downstream_boundary < self.downstream_boundary:
                 self.downstream_boundary = read.downstream_boundary
     
-    def does_contain(self, read, distance):
+    def is_consistent(self, read, distance):
         if self.chrom == read.chrom_1 and read.start_1 < self.last_pos + distance:
-            pass
             if self.insertion_strand == read.insertion_strand:
-                pass
+                if len(self.down_reads) == 0:
+                    return True
+                elif read.position == "down":
+                    return True
+                else:
+                    return False
+            else:
+                return False
         else:
             return False
 
+
+def close_insertions(insertion1, insertion2, distance):
+    if insertion1.chrom == insertion2.chrom and abs(insertion1.last_pos - insertion2.last_pos) < distance:
+        return True
+    else:
+        return False
 
 def summarize_insertions(bedpe_file, out, failed, tsd, distance):
     '''
@@ -80,64 +91,95 @@ def summarize_insertions(bedpe_file, out, failed, tsd, distance):
                 continue
 
             read = Read(line)
-            if len(insertions) > 0 and insertions[-1].does_contain(read, distance):
-                insertions[-1].add_read(read)
-            else:
+            compatible_insertions = [i for i in insertions if i.is_consistent(read, distance)]
+            if len(compatible_insertions) == 0:
                 insertion = Insertion()
                 insertion.add_read(read)
                 insertions.append(insertion)
-
-
-            if chrom_1 == curr_chr and start_1 < int(curr_pos) + distance:
-                # existing insertion
-                curr_pos = end_1
-                line_insertion = "-" if strand_1 == strand_2 else '+'
-                curr_num_reads += 1
-                if strand_1 == '+' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
-                    curr_left_softclip.append(line.strip())
-                if strand_1 == '-' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
-                    curr_right_softclip.append(line.strip())
-                if (curr_strand == "-" and strand_1 == '+') or (line_insertion != insertion_strand):
-                    inconsistent_read += 1
-                    failed_list.append(line.strip())
-                    # next try to report inconsistent reads per insertion.
-                else:
-                    insertion_strand = line_insertion
-                    curr_strand = strand_1
-                    consistent_read += 1
             else:
-                # new insertion
-                if len(curr_left_softclip) > 0 and len(curr_right_softclip) > 0:
-                    # report soft-clipped reads
-                    tsd_list.extend(curr_left_softclip + curr_right_softclip)
-                curr_left_softclip = []
-                curr_right_softclip = []
-                if strand_1 == '+' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
-                    curr_left_softclip.append(line.strip())
-                if strand_1 == '-' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
-                    curr_right_softclip.append(line.strip())
-                curr_chr = chrom_1
-                curr_pos = end_1
-                insertion_strand = "-" if strand_1 == strand_2 else '+'
-                curr_strand = strand_1
-                insertion_number += 1 if curr_num_reads > 1 else 0
-                curr_num_reads = 1
+                compatible_insertions[0].add_read(read)
 
-        if len(curr_left_softclip) > 0 and len(curr_right_softclip) > 0:
-            # report soft-clipped reads
-            tsd_list.append(curr_left_softclip + curr_right_softclip)
-        insertion_number += 1 if curr_num_reads > 1 else 0
+    consistent_insertions = []
+    tbd_insertions = []
+    for index, insertion in enumerate(insertions):
+        if index == 0:
+            if close_insertions(insertion, insertions[index + 1], distance):
+               tbd_insertions.append(insertion)
+            else:
+                consistent_insertions.append(insertion)
+        elif index == len(insertions) - 1:
+            if close_insertions(insertion, insertions[index - 1], distance):
+                tbd_insertions.append(insertion)
+            else:
+                consistent_insertions.append(insertion)
+        else:
+            if close_insertions(insertion, insertions[index - 1], distance) or close_insertions(insertion, insertions[index + 1], distance):
+                tbd_insertions.append(insertion)
+            else:
+                consistent_insertions.append(insertion)
 
     with open(out, 'w') as f:
-        f.write(f"{insertion_number}\t{consistent_read}\t{inconsistent_read}\n")
-    if failed:
-        with open(failed, 'w') as failed_out:
-            for failed_line in failed_list:
-                failed_out.write(f"{failed_line}\n")
-    if tsd:
-        with open(tsd, 'w') as tsd_out:
-            for tsd_line in tsd_list:
-                tsd_out.write(f"{tsd_line}\n")
+        for insertion in consistent_insertions:
+            total_reads = len(insertion.up_reads) + len(insertion.down_reads)
+            sum_line = f"{insertion.chrom}\t{insertion.upstream_boundary}\t{insertion.downstream_boundary}\t{insertion.insertion_strand}\t{total_reads}\n"
+            f.write(sum_line)
+
+    with open(out, 'w') as f:
+        for insertion in tbd_insertions:
+            total_reads = len(insertion.up_reads) + len(insertion.down_reads)
+            sum_line = f"{insertion.chrom}\t{insertion.upstream_boundary}\t{insertion.downstream_boundary}\t{insertion.insertion_strand}\t{total_reads}\n"
+            f.write(sum_line)
+
+            # if chrom_1 == curr_chr and start_1 < int(curr_pos) + distance:
+            #     # existing insertion
+            #     curr_pos = end_1
+            #     line_insertion = "-" if strand_1 == strand_2 else '+'
+            #     curr_num_reads += 1
+            #     if strand_1 == '+' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
+            #         curr_left_softclip.append(line.strip())
+            #     if strand_1 == '-' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
+            #         curr_right_softclip.append(line.strip())
+            #     if (curr_strand == "-" and strand_1 == '+') or (line_insertion != insertion_strand):
+            #         inconsistent_read += 1
+            #         failed_list.append(line.strip())
+            #         # next try to report inconsistent reads per insertion.
+            #     else:
+            #         insertion_strand = line_insertion
+            #         curr_strand = strand_1
+            #         consistent_read += 1
+            # else:
+            #     # new insertion
+            #     if len(curr_left_softclip) > 0 and len(curr_right_softclip) > 0:
+            #         # report soft-clipped reads
+            #         tsd_list.extend(curr_left_softclip + curr_right_softclip)
+            #     curr_left_softclip = []
+            #     curr_right_softclip = []
+            #     if strand_1 == '+' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
+            #         curr_left_softclip.append(line.strip())
+            #     if strand_1 == '-' and (re.search(r'(\d+)S', cigar_1) or re.search(r'(\d+)S', cigar_2)):
+            #         curr_right_softclip.append(line.strip())
+            #     curr_chr = chrom_1
+            #     curr_pos = end_1
+            #     insertion_strand = "-" if strand_1 == strand_2 else '+'
+            #     curr_strand = strand_1
+            #     insertion_number += 1 if curr_num_reads > 1 else 0
+            #     curr_num_reads = 1
+
+        # if len(curr_left_softclip) > 0 and len(curr_right_softclip) > 0:
+        #     # report soft-clipped reads
+        #     tsd_list.append(curr_left_softclip + curr_right_softclip)
+        # insertion_number += 1 if curr_num_reads > 1 else 0
+
+    # with open(out, 'w') as f:
+    #     f.write(f"{insertion_number}\t{consistent_read}\t{inconsistent_read}\n")
+    # if failed:
+    #     with open(failed, 'w') as failed_out:
+    #         for failed_line in failed_list:
+    #             failed_out.write(f"{failed_line}\n")
+    # if tsd:
+    #     with open(tsd, 'w') as tsd_out:
+    #         for tsd_line in tsd_list:
+    #             tsd_out.write(f"{tsd_line}\n")
 
 
 def main():
