13f2bafc68c50fb7d9ceb7963725d3a559940b5c
xzhuo <yunkzo@gmail.com>
2026-05-07 11:11:11 -0500

    fix mm39 bug

--- genome.sh
+++ genome.sh
@@ -6,7 +6,7 @@ source config.sh
 mkdir -p $GENOME_DIR
 cd $GENOME_DIR
 ln -s $KRAKEN_DB/library/9606/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_genomic.fna .
-ln -s $KRAKEN_DB/library/10090/genomes/all/GCF/000/001/635/GCF/000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna .
+ln -s $KRAKEN_DB/library/10090/genomes/all/GCF/000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna .
 cat GCF_009914755.1_T2T-CHM13v2.0_genomic.fna GCF_000001635.27_GRCm39_genomic.fna > combined_genome.fna
 samtools faidx GCF_009914755.1_T2T-CHM13v2.0_genomic.fna
 samtools faidx GCF_000001635.27_GRCm39_genomic.fna
